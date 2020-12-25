06401164a3413c5039f7076ee8b34a81a2ee1067
jboruk <jakub.boruk@wp.pl>
2020-12-25 13:30:38 +0100

    added dropdowns to navigation

--- src/components/constants/routings.js
+++ src/components/constants/routings.js
@@ -3,6 +3,19 @@ class Routings {
     static CLOTHING = '/clothing'
     static SHOES = '/shoes'
     static HOME = '/home'
+    static T_SHIRTS = '/tshirts'
+    static HOODIES = '/hoodies'
+    static TROUSERS = '/trousers'
+    static SHIRTS = '/tshirts'
+    static UNDERWEAR = '/underwear'
+    static SPORT_SHOES = '/sport'
+    static ELEGANT_SHOES = '/elegant'
+    static BOOTS = '/boots'
+    static FLIP_FLOPS = '/flipFlops'
+    static BACKPACKS = '/backpacks'
+    static HATS = '/hats'
+    static WATCHES = '/watches'
+    static BELTS = '/belts'
 }
 
 export default Routings;
--- src/components/nav/MainNav.js
+++ src/components/nav/MainNav.js
@@ -1,27 +1,116 @@
 import React, { useState } from 'react';
-import { Button, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
+import { Button, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Routings from '../constants/routings';
 
+const inheritStyle = {
+    color: 'inherit'
+}
+
 const MainNav = () => {
-    const [searchText, setSearchText] = useState();
+    const [searchText, setSearchText] = useState('');
+    const [showClothingDropdown, setShowClothingDropdown] = useState(false);
+    const [showShoesDropdown, setShowShoesDropdown] = useState(false);
+    const [showAccessoriesDropdown, setShowAccessoriesDropdown] = useState(false);
+
+    const toggleClothingDropdown = () => {
+        setShowClothingDropdown(!showClothingDropdown);
+    }
+
+    const toggleShoesDropdown = () => {
+        setShowShoesDropdown(!showShoesDropdown);
+    }
+
+    const toggleAccessoriesDropdown = () => {
+        setShowAccessoriesDropdown(!showAccessoriesDropdown);
+    }
 
     return (
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand>
-                <Link to={Routings.HOME} style={{color: 'inherit'}}>CiuchBazar</Link>
+                <Link to={Routings.HOME} style={inheritStyle}>CiuchBazar</Link>
             </Navbar.Brand>
 
             <Nav className="mr-auto">
-                <Nav.Link>
-                    <Link to={Routings.CLOTHING} style={{color: 'inherit'}}>Odzież</Link>
-                </Nav.Link>
-                <Nav.Link>
-                    <Link to={Routings.SHOES} style={{color: 'inherit'}}>Obuwie</Link>
-                </Nav.Link>
-                <Nav.Link>
-                    <Link to={Routings.ACCESSORIES} style={{color: 'inherit'}}>Akcesoria</Link>
-                </Nav.Link>
+                <NavDropdown title="Odzież" id="collasible-nav-dropdown"
+                    onMouseEnter={toggleClothingDropdown} 
+                    onMouseLeave={toggleClothingDropdown}
+                    show={showClothingDropdown}
+                >
+                    <NavDropdown.Item>
+                        <Link to={Routings.CLOTHING} style={inheritStyle}>Wszystkie Kategorie</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                        <Link to={Routings.CLOTHING + Routings.T_SHIRTS} style={inheritStyle}>Koszulki</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                        <Link to={Routings.CLOTHING + Routings.HOODIES} style={inheritStyle}>Bluzy</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                        <Link to={Routings.CLOTHING + Routings.TROUSERS} style={inheritStyle}>Spodnie</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                        <Link to={Routings.CLOTHING + Routings.SHIRTS} style={inheritStyle}>Koszule</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                        <Link to={Routings.CLOTHING + Routings.UNDERWEAR} style={inheritStyle}>Bielizna</Link>
+                    </NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Obuwie" id="collasible-nav-dropdown"
+                    onMouseEnter={toggleShoesDropdown} 
+                    onMouseLeave={toggleShoesDropdown}
+                    show={showShoesDropdown}
+                >
+                    <NavDropdown.Item>
+                        <Link to={Routings.SHOES} style={inheritStyle}>Wszystkie Kategorie</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                        <Link to={Routings.SHOES + Routings.SPORT_SHOES} style={inheritStyle}>Obuwie sportowe</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                        <Link to={Routings.SHOES + Routings.BOOTS} style={inheritStyle}>Półbuty</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                        <Link to={Routings.SHOES + Routings.ELEGANT_SHOES} style={inheritStyle}>Obuwie eleganckie</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                        <Link to={Routings.SHOES + Routings.FLIP_FLOPS} style={inheritStyle}>Klapi</Link>
+                    </NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Akcesoria" id="collasible-nav-dropdown"
+                    onMouseEnter={toggleAccessoriesDropdown} 
+                    onMouseLeave={toggleAccessoriesDropdown}
+                    show={showAccessoriesDropdown}
+                >
+                    <NavDropdown.Item>
+                        <Link to={Routings.ACCESSORIES} style={inheritStyle}>Wszystkie Kategorie</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                        <Link to={Routings.ACCESSORIES + Routings.BACKPACKS} style={inheritStyle}>Plecaki</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                        <Link to={Routings.ACCESSORIES + Routings.HATS} style={inheritStyle}>Czapki</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                        <Link to={Routings.ACCESSORIES + Routings.WATCHES} style={inheritStyle}>Zegarki</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item>
+                        <Link to={Routings.ACCESSORIES + Routings.BELTS} style={inheritStyle}>Paski</Link>
+                    </NavDropdown.Item>
+                </NavDropdown>
             </Nav>
             
             <Form inline onSubmit={(event) => {
